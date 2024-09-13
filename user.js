70bc77c2740fa2197a7f6d8fbcb7136ba3ee6b8e
Vedanti1703 <165309817+Vedanti1703@users.noreply.github.com>
2024-09-13 23:31:10 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,15 +1,52 @@
-const { required } = require("joi");
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
-const passportLocalMongoose=require("passport-local-mongoose");
+const express = require("express");
+const router = express.Router();
+const User=require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport=require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
-const userSchema=new Schema({
-    email:{
-        type:String,
-        required:true
-    },
+router.get("/signup",(req,res)=>{
+    res.render("users/signup.ejs");
 });
 
-userSchema.plugin(passportLocalMongoose);
+router.post("/signup",wrapAsync(async(req,res)=>{
+    try{
+        let {username,email,password}=req.body;
+        const newUser=new User({email,username});
+        const registeredUser=await User.register(newUser,password);
+        req.login(registeredUser,(err)=>{
+if(err){
+    return next(err);
+}
+req.flash("success","User was registered");
+res.redirect("/listings");
+        });
 
-module.exports = mongoose.model('User', userSchema);
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+ 
+}));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),async(req,res)=>{
+req.flash("success","Welcome back to Wanderlust!.You are logged in.");
+let redirectUrl=res.locals.redirectUrl || "/listings";
+res.redirect(redirectUrl);
+});
+
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("succcess","You are logged out");
+        res.redirect("/listings");
+    })
+});
+
+module.exports = router;
